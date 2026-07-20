5452d8f28233184ead04fe9032d9547666780228
muhammadusmanrasheed0 <ranahasaanahmad@gmail.com>
2026-07-20 17:59:03 +0500

    Macros/Context & python operator

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -1,25 +1,3 @@
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
-# from datetime import datetime
-
-# def print_hello():
-#     print("Hello from Apache Airflow! 🚀")
-#     print("My first DAG is working successfully.")
-
-# with DAG(
-#     dag_id="hello_world_dag",
-#     start_date=datetime(2026, 7, 19),
-#     schedule="@hourly",
-#     catchup=True,
-#     tags=["example", "learning"],
-# ) as dag:
-
-#     hello_task = PythonOperator(
-#         task_id="hello_task",
-#         python_callable=print_hello,
-#     )
-
-#     hello_task
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
