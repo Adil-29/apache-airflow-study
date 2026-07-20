3e871fb4a0b8d818a1b593dfa081a954c6d435b3
muhammadusmanrasheed0 <ranahasaanahmad@gmail.com>
2026-07-20 17:52:42 +0500

    macos/pythonoperator

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -1,22 +1,44 @@
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
+
+# def print_hello():
+#     print("Hello from Apache Airflow! 🚀")
+#     print("My first DAG is working successfully.")
+
+# with DAG(
+#     dag_id="hello_world_dag",
+#     start_date=datetime(2026, 7, 19),
+#     schedule="@hourly",
+#     catchup=True,
+#     tags=["example", "learning"],
+# ) as dag:
+
+#     hello_task = PythonOperator(
+#         task_id="hello_task",
+#         python_callable=print_hello,
+#     )
+
+#     hello_task
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def print_hello():
-    print("Hello from Apache Airflow! 🚀")
-    print("My first DAG is working successfully.")
+def print_context(**context):
+    print("Logical Date:", context["logical_date"])
+    print("Run ID:", context["run_id"])
+    print("Task ID:", context["task"].task_id)
+    print("DAG ID:", context["dag"].dag_id)
+    print("Execution Date (ds):", context["ds"])
 
 with DAG(
     dag_id="hello_world_dag",
-    start_date=datetime(2026, 7, 19),
+    start_date=datetime(2026, 7, 20),
     schedule="@hourly",
     catchup=True,
-    tags=["example", "learning"],
 ) as dag:
 
     hello_task = PythonOperator(
         task_id="hello_task",
-        python_callable=print_hello,
+        python_callable=print_context,
     )
-
-    hello_task
